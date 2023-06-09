1921d783dd141d5a0721ed64dd4ba8d204ff3350
Alexey <stud0000250583@study.utmn.ru>
2023-06-09 18:46:02 +0500

    fix formatting

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
-<?php require 'hello.php'?>
+<?php require 'hello.php' ?>
 
 <!DOCTYPE html>
 <html lang="ru">
 
 <head>
     <meta charset="UTF-8">
-    <?=$title?>
+    <?= $title ?>
 </head>
 
 <body>
-    <?=$header?>
-    <p>Время обращения: <?=get_current_time()?></p>
-    <p>Дата обращения: <?=get_current_date()?></p>
+    <?= $header ?>
+    <p>Время обращения:
+        <?= get_current_time() ?>
+    </p>
+    <p>Дата обращения:
+        <?= get_current_date() ?>
+    </p>
 </body>
 
 </html>
