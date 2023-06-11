1809e8836b197f257a656f4ad120a8c52fd626a7
Alexey <stud0000250583@study.utmn.ru>
2023-06-11 19:17:57 +0500

    set max length for name and message

--- templates/catalog_entry.php
+++ templates/catalog_entry.php
@@ -18,8 +18,8 @@
         <h2>Отзывы</h2>
         <input type="hidden" name="id" value="<?= $url_array = explode('/', $_SERVER['REQUEST_URI'])[2] ?>" />
         <p>Оставьте отзыв:</p>
-        <input type="text" name="name" placeholder="Ваше имя" />
-        <input type="text" name="message" placeholder="Ваш отзыв" />
+        <input type="text" maxlength="250" name="name" placeholder="Ваше имя" />
+        <input type="text" maxlength="250" name="message" placeholder="Ваш отзыв" />
         <input type="submit" value="Добавить" />
     </form>
 
@@ -33,7 +33,7 @@
                     <?= $review['name'] ?>:
                 </b>
                 <div class="feedback_message">
-                    <textarea name="message" rows="3"><?= $review['contents'] ?></textarea>
+                    <textarea maxlength="250" name="message" rows="3"><?= $review['contents'] ?></textarea>
                 </div>
                 <input formmethod="post" formaction="/models/feedback.php?action=update" type="submit" value="Сохранить" />
                 <input formmethod="post" formaction="/models/feedback.php?action=delete" type="submit" value="Удалить" />
